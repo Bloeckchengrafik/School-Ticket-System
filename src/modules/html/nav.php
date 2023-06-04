177446ad2f95e022ce35287b84d3814f666f4e8f
bergschneider <magiclake@gmx.de>
2023-06-04 22:44:10 +0200

    feat: better nav

--- src/modules/html/nav.php
+++ src/modules/html/nav.php
@@ -1,7 +1,15 @@
 <?php
 include_once "modules/html/init.php";
+
+use modules\auth\AccountType;
 use modules\auth\Session as sess;
+use modules\auth\User;
+
 global $page;
+
+/* @var $user User
+ */
+$user = sess::parseUser();
 ?>
 
 <nav>
@@ -13,23 +21,28 @@ global $page;
             </a>
 
             <ul class="navbar">
-                <li class="navitem <?php if ($page == "dashboard") { echo "active"; } ?>"><a href="#">Dashboard</a></li>
-                <li class="navitem <?php if ($page == "tickets") { echo "active"; } ?>"><a href="#">Meine Tickets</a></li>
-                <li class="navitem <?php if ($page == "newticket") { echo "active"; } ?>"><a href="#">Neues Ticket</a></li>
+                <li class="navitem <?php if ($page == "dashboard") {
+                    echo "active";
+                } ?>"><a href="/">Dashboard</a></li>
+                <li class="navitem <?php if ($page == "tickets") {
+                    echo "active";
+                } ?>"><a href="#">Meine Tickets</a></li>
+                <li class="navitem <?php if ($page == "newticket") {
+                    echo "active";
+                } ?>"><a href="#">Neues Ticket</a></li>
+                <?php if ($user->accountClass() == AccountType::Admin) { ?>
+                    <li class="navitem <?php if ($page == "admin") {
+                        echo "active";
+                    } ?>"><a href="#">Benutzer verwalten</a></li>
+                <?php } ?>
             </ul>
         </div>
         <div>
             <img class="profilepicture" src="<?php
-            $user = sess::parseUser();
-
-            if ($user == null) {
-                echo "";
-            } else {
-                // Hash the email to prevent direct access to the profile picture
-                $profilePictureSeed = md5($user->userID . $user->email);
-                echo "https://rest.devstorage.eu/user/avatar/" .
-                    $profilePictureSeed;
-            }
+            // Hash the email to prevent direct access to the profile picture
+            $profilePictureSeed = md5($user->userID . $user->email);
+            echo "https://rest.devstorage.eu/user/avatar/" .
+                $profilePictureSeed;
             ?>" alt="PROFILE" height="32">
             <div class="user">
                 <span>
